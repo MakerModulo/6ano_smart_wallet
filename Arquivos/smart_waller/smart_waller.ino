fa5565295a8e5a6a62a693cb817500da660517f6
Leo Ruiz <ruizruizleo@hotmail.com>
2021-12-07 16:40:20 -0300

    eeprom

--- Arquivos/smart_waller/smart_waller.ino
+++ Arquivos/smart_waller/smart_waller.ino
@@ -1,5 +1,7 @@
+#include <EEPROM.h>
+
 int contador = 0;
-float valor = 0;
+float valor = EEPROM.read(0);
 float valores[] = {1.0,0.50,0.25,0.10,0.05};
 
 unsigned long lastDebounceTime = 0;
@@ -10,12 +12,16 @@ unsigned long debounceDelay = 250;
 LiquidCrystal_I2C lcd(0x27,20,4);
 
 void setup() {
+  zerarCofre();
   lcd.init();
+  lcd.clear();
   lcd.backlight();
   lcd.setCursor(2,0);
   lcd.print("Smart wallet");
   lcd.setCursor(2,1);
-  lcd.print("R$ 0.00");
+  lcd.print("R$ ");
+  lcd.setCursor(5,1);
+  lcd.print(valor);
   Serial.begin(115200);
   pinMode(3, INPUT);
   pinMode(13, OUTPUT);
@@ -39,7 +45,15 @@ void loop() {
       lcd.print("R$");
       lcd.setCursor(5,1);
       lcd.print(valor);
+      EEPROM.write(0, valor);
       lastDebounceTime = millis();
     }
   }
+}
+
+void zerarCofre(){
+  EEPROM.write(0, 0);
+  valor = EEPROM.read(0);
+  lcd.setCursor(5,1);
+  lcd.print("0.00");
 }
